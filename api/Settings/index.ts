2a8d0c99a5b1f315d619db0f54512e32ad67b9bb
Bo Sandorf Pluszek <b205093@al-bank.dk>
2022-04-06 08:14:06 +0200

    add api

---
+++ api/Settings/index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    context.res.json({
+        test:123
+    });
+
+};
+
+export default httpTrigger;
